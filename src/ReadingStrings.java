9c84dd0534f2e10f100e18a572ff1bb2e523836e
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-20 20:14:37 -0600

    Initial commit

---
+++ src/ReadingStrings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Reading Strings {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        {
+            System.out.println(" Enter the strings:");
+            String s1;
+            s1 = input.next();
+            String s2 = input.next();
+
+            System.out.println("s1 = " + s1);
+            System.out.println("s2 = " + s2);
+        }
+    }
+}
